bf1fb675c253d1dee3dac7af6b53e94629415b3f
radigu74 <radigu74@yahoo.com>
2026-06-17 01:25:39 +0800

    Use explicit secure Gmail SMTP settings

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,13 +2,18 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, text, html }) => {
   
-  const transporter = nodemailer.createTransport({
-    service: "Gmail", // Or SMTP details
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 60000,
+  greetingTimeout: 60000,
+  socketTimeout: 60000,
+});
 
   const mailOptions = {
     from: `"${process.env.APP_NAME}" <${process.env.EMAIL_USER}>`,
