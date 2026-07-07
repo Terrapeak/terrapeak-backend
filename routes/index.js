2991023aca879e54cc2f6a3fcd11f47bc5c4014b
Terrapeak <connect@terrapeakgroup.com>
2026-07-07 18:20:38 +0800

    Connect Facebook route to API router

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import chatbotRoutes from "./chatbot.js";
 import userRoutes from "./user.js";
 import appointmentRoutes from "./appointment.js";
 import widgetRouter from "./widget.js";
+import facebookRoutes from "./facebook.js";
+
 const router = express.Router();
 
 router.use("/auth", authRoutes);
@@ -11,5 +13,6 @@ router.use("/chatbot", chatbotRoutes);
 router.use("/admin/users", userRoutes);
 router.use("/appointments", appointmentRoutes);
 router.use("/api", widgetRouter);
+router.use("/facebook", facebookRoutes);
 
 export default router;
