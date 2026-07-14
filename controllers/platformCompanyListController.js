c52dbfd2ec2e562e674a8708cd1d3497c049602e
Terrapeak <connect@terrapeakgroup.com>
2026-07-14 22:52:20 +0800

    Add platform customer list endpoint

---
+++ controllers/platformCompanyListController.js
@@ -0,0 +1,17 @@
+import asyncHandler from "express-async-handler";
+import Company from "../models/company.js";
+
+export const listPlatformCompanies = asyncHandler(async (req, res) => {
+  const query = (req.query.q || "").trim();
+
+  const filter = query
+    ? {
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { displayName: { $regex: query, $options: "i" } },
+          { slug: { $regex: query, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const companies = await Company.find(filter)
