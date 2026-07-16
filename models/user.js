ecc8f26d88f6cfccf65cb9095cbdde6ebe964057
Terrapeak <connect@terrapeakgroup.com>
2026-07-16 08:59:43 +0800

    Add invitation and password reset fields

--- models/user.js
+++ models/user.js
@@ -6,48 +6,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   phone: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  country: { type: String, required: false }, // Added country field
-  companyName: { type: String, required: false }, // Added company name field
+  country: { type: String, required: false },
+  companyName: { type: String, required: false },
   isAdmin: { type: Boolean, default: false },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-    required: true,
-  },
-
-  platformRole: {
-  type: String,
-  enum: [
-    "none",
-    "platform-owner",
-    "platform-admin",
-    "support-admin",
-    "billing-admin",
-    "developer-admin",
-    "sales-admin",
-    "viewer",
-  ],
-  default: "none",
-},
-
-  isApproved: { type: Boolean, default: false },
-  isGoogleOauth:{type:Boolean,default:false},
-  googleRefreshToken: { type: String },
-  googleAccessToken:{type:String},
-});
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const User = mongoose.model("User", userSchema);
-
-export default User;
