ce2f51d65a4f62f1111d0bf689f99f368002e370
Terrapeak <connect@terrapeakgroup.com>
2026-07-16 14:24:48 +0800

    Mount support messaging routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./user.js";
 import appointmentRoutes from "./appointment.js";
 import companyRoutes from "./company.js";
 import platformAdminRoutes from "./platformAdmin.js";
+import supportRoutes from "./support.js";
 import widgetRouter from "./widget.js";
 import facebookRoutes from "./facebook.js";
 
@@ -16,6 +17,7 @@ router.use("/admin/users", userRoutes);
 router.use("/appointments", appointmentRoutes);
 router.use("/company", companyRoutes);
 router.use("/platform-admin", platformAdminRoutes);
+router.use("/support", supportRoutes);
 router.use("/api", widgetRouter);
 router.use("/facebook", facebookRoutes);
 
