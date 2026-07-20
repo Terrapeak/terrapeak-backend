53bfa051e7a61c338bb2fb56ae71079e2e2cd193
Terrapeak <connect@terrapeakgroup.com>
2026-07-20 08:39:37 +0800

    Add company-scoped Facebook channel config model

---
+++ models/facebookChannelConfig.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const FacebookChannelConfigSchema = new mongoose.Schema(
+  {
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    appInstallationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CompanyAppInstallation",
+      default: null,
+    },
+    connectionStatus: {
+      type: String,
+      enum: ["not_connected", "connecting", "connected", "error"],
+      default: "not_connected",
+    },
+    pageId: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    pageName: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    pageAccessTokenEncrypted: {
+      type
