c84fc7e690e78eb875fc1c1c131e0da21876f0c2
Terrapeak <connect@terrapeakgroup.com>
2026-07-20 10:26:28 +0800

    Restore complete support saved reply model

--- models/supportSavedReply.js
+++ models/supportSavedReply.js
@@ -10,4 +10,13 @@ const SupportSavedReplySchema = new mongoose.Schema(
       index: true,
     },
     body: { type: String, required: true, trim: true, maxlength: 10000 },
-    createdByUserId: { type: mongoose.Schema.Types.ObjectId,
+    createdByUserId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    updatedByUserId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+    isActive: { type: Boolean, default: true, index: true },
+  },
+  { timestamps: true }
+);
+
+SupportSavedReplySchema.index({ title: 1, category: 1 });
+
+export default mongoose.model("SupportSavedReply", SupportSavedReplySchema);
