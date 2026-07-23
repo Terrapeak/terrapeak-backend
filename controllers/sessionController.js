49d73348601184ad4e638cd48704d703bfe2e3ee
Terrapeak <connect@terrapeakgroup.com>
2026-07-23 19:48:02 +0800

    Add dashboard and platform session validation endpoints

---
+++ controllers/sessionController.js
@@ -0,0 +1,35 @@
+import asyncHandler from "express-async-handler";
+
+const buildDashboardUser = (user) => ({
+  _id: user._id,
+  name: user.name,
+  email: user.email,
+  phone: user.phone,
+  country: user.country,
+  companyName: user.companyName,
+  isAdmin: user.isAdmin,
+  isApproved: user.isApproved,
+  role: user.role || "user",
+  platformRole: user.platformRole || "none",
+});
+
+const buildPlatformUser = (user) => ({
+  _id: user._id,
+  name: user.name,
+  email: user.email,
+  platformRole: user.platformRole,
+});
+
+export const getDashboardSession = asyncHandler(async (req, res) => {
+  return res.status(200).json({
+    success: true,
+    user: buildDashboardUser(req.user),
+  });
+});
+
+export const getPlatformSession = asyncHandler(async (req, res) => {
+  return res.status(200).json({
+    success: true,
+    platformUser: buildPlatformUser(req.platformUser),
+  });
+});
