d8a9c03d1854f18814b3c0f6b1d7309221ffa295
Terrapeak <connect@terrapeakgroup.com>
2026-07-24 12:54:42 +0800

    Exclude internal workspace from customer list

--- controllers/platformCompanyListController.js
+++ controllers/platformCompanyListController.js
@@ -3,16 +3,18 @@ import Company from "../models/company.js";
 
 export const listPlatformCompanies = asyncHandler(async (req, res) => {
   const query = (req.query.q || "").trim();
+  const customerFilter = { isPlatformWorkspace: { $ne: true } };
 
   const filter = query
     ? {
+        ...customerFilter,
         $or: [
           { name: { $regex: query, $options: "i" } },
           { displayName: { $regex: query, $options: "i" } },
           { slug: { $regex: query, $options: "i" } },
         ],
       }
-    : {};
+    : customerFilter;
 
   const companies = await Company.find(filter)
     .select(
