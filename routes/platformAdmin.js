58edf742bb1adc2cbc3e0f7674c8f562cf779d9c
Terrapeak <connect@terrapeakgroup.com>
2026-07-27 12:26:08 +0800

    Expose platform company user lifecycle list

--- routes/platformAdmin.js
+++ routes/platformAdmin.js
@@ -21,6 +21,7 @@ import {
   resendPlatformCompanyInvitation,
   sendPlatformUserPasswordReset,
 } from "../controllers/platformUserLifecycleController.js";
+import { listPlatformCompanyUsers } from "../controllers/platformCompanyUserListController.js";
 import { updatePlatformCommercialSettings } from "../controllers/platformCommercialController.js";
 import {
   getPlatformAIConfig,
@@ -49,6 +50,7 @@ router.get("/onboarding/options", isPlatformAuthenticated, isPlatformAdmin, getP
 router.post("/onboarding", isPlatformAuthenticated, isPlatformAdmin, onboardPlatformCustomer);
 router.get("/companies/search", isPlatformAuthenticated, isPlatformAdmin, searchPlatformCompanies);
 router.get("/companies/:companyId", isPlatformAuthenticated, isPlatformAdmin, getPlatformCompanyDetail);
+router.get("/companies/:companyId/users", isPlatformAuthenticated, isPlatformAdmin, listPlatformCompanyUsers);
 router.get("/companies/:companyId/effective-billing", isPlatformAuthenticated, isPlatformAdmin, getPlatformCompanyEffectiveBilling);
 router.patch("/companies/:companyId", isPlatformAuthenticated, isPlatformAdmin, updatePlatformCompany);
 router.patch("/companies/:companyId/commercial", isPlatformAuthenticated, isPlatformAdmin, updatePlatformCommercialSettings);
