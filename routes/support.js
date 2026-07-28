d3ac47db34ccfcc879a0049a6c6769c1bcac63cc
Terrapeak <connect@terrapeakgroup.com>
2026-07-28 11:24:54 +0800

    Require verified users for customer support routes

--- routes/support.js
+++ routes/support.js
@@ -1,6 +1,7 @@
 import express from "express";
 import isPlatformAuthenticated from "../middleware/isPlatformAuthenticated.js";
 import isPlatformAdmin from "../middleware/isPlatformAdmin.js";
+import isVerifiedUser from "../middleware/isVerifiedUser.js";
 import resolveCompanyContext from "../middleware/resolveCompanyContext.js";
 import {
   listMySupportConversations,
@@ -26,10 +27,12 @@ import { getSupportReport } from "../controllers/supportReportController.js";
 
 const router = express.Router();
 
-router.get("/conversations", resolveCompanyContext, listMySupportConversations);
-router.post("/conversations", resolveCompanyContext, createSupportConversation);
-router.post("/conversations/:conversationId/messages", resolveCompanyContext, replyToMySupportConversation);
-router.delete("/conversations/:conversationId", resolveCompanyContext, hideMySupportConversation);
+const customerAuth = [isVerifiedUser, resolveCompanyContext];
+
+router.get("/conversations", ...customerAuth, listMySupportConversations);
+router.post("/conversations", ...customerAuth, createSupportConversation);
+router.post("/conversations/:conversationId/messages", ...customerAuth, replyToMySupportConversation);
+router.delete("/conversations/:conversationId", ...customerAuth, hideMySupportConversation);
 
 const platformAuth = [isPlatformAuthenticated, isPlatformAdmin];
 
